85f3ac020acb18a93ace0f97eabc5af41d47c4e5
Nanna <nanhe@itu.dk>
2025-09-02 22:31:22 +0200

    Implemented StreamReader to read .csv files and hereafter print the cheeps

--- Program.cs
+++ Program.cs
@@ -1,4 +1,38 @@
-﻿foreach (var arg in args)
+﻿string path = @"C:\Users\nanhe\chirp\Chirp.CLI\chirp_cli_db.csv";
+
+
+if (File.Exists(path))
 {
-    Console.WriteLine(arg);
+    StreamReader sr = new StreamReader(File.OpenRead(path));
+
+    while (!sr.EndOfStream)
+    {
+        var line = sr.ReadLine();
+        var values = line.Split(',');
+        string user = null;
+        string mes = null;
+        string time = null;
+        if (values.Length > 3)
+        {
+            user = values[0];
+            mes = values[1] + values[2];
+            time = values[3];
+        }
+        else
+        {
+            user = values[0];
+            mes = values[1];
+            time = values[2];
+        }
+        if (!(user == "Author"))
+        {
+            Console.WriteLine(user + " @ " + time + ": " + mes);
+        }
+    }
+
+    sr.Close();
+}
+else
+{
+    Console.WriteLine("Files does not exist");
 }
