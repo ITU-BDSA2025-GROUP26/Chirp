92c1d08b9f451f0168054df296c12979edf812d9
Victor <vitr@itu.dk>
2025-09-05 12:00:45 +0200

    Project work implementing simpleDB. Not finished yet.

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleDB;
+
+public sealed class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        return new String[1] { "Hello" };
+    }
+
+    public void Store(T record)
+    {
+        
+    }
+}
