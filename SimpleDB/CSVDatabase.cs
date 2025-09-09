34b3143f72d48cf05991e32871b82c61a401b66a
Victor <vitr@itu.dk>
2025-09-09 16:30:16 +0200

    Implementation of SimpleDB

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -4,7 +4,7 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
     public IEnumerable<T> Read(int? limit = null)
     {
-        return new String[1] { "Hello" };
+        
     }
 
     public void Store(T record)
