03f002ddd7a81508b34331bb8ec66b16d584b25f
Victor <vitr@itu.dk>
2025-09-19 11:16:13 +0200

    Refactoring SimpleDB into Chirp.CLI to make it possible to run a Azure server, since only one csproj file was allowed.

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -38,33 +38,5 @@ class Program
         });
 
         app.Run();
-
-        /*
-
-        var rootCommand = new RootCommand("Chirp.CLI - a simple microblogging tool");
-
-        var readCommand = new Command("read", "Read all cheeps");
-        readCommand.SetHandler(() =>
-        {
-            foreach (var c in database.Read())
-                UserInterface.ShowCheep(c);
-        });
-        rootCommand.AddCommand(readCommand);
-
-        var messageArg = new Argument<string>("message", "Message to post");
-        var cheepCommand = new Command("cheep", "Post a new cheep");
-        cheepCommand.AddArgument(messageArg);
-
-        cheepCommand.SetHandler((string message) =>
-        {
-            var author = Environment.UserName;
-            var ts = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-            database.Store(new Cheep(author, message, ts));
-        }, messageArg);
-
-        rootCommand.AddCommand(cheepCommand);
-
-        return await rootCommand.InvokeAsync(args);
-        */
     }
 }
