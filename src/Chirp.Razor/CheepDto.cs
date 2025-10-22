5b31e185115c370acc45e36a9c1831bbb1171117
Victor <vitr@itu.dk>
2025-10-22 10:48:08 +0200

    Refactoring DBFacade to its own classes. Co-authored-by: Nanna <nanhe@itu.dk>

---
+++ src/Chirp.Razor/CheepDto.cs
@@ -0,0 +1,8 @@
+namespace Chirp.Razor;
+
+public class CheepDto
+{
+    public string Text { get; set; }
+    public string Author { get; set; }
+    public int UnixTimeStamp { get; set; } //how to format ???
+}
