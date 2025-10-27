a40c0041532caaacfe1d24e9f2c84ba6a39234e9
Nanna <nanhe@itu.dk>
2025-10-27 14:30:16 +0100

    Added the four new methods to the ICheepRepository

--- src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
@@ -1,8 +1,13 @@
 using Chirp.Core;
+using Chirp.Core.Models;
 namespace Chirp.Infrastructure.Chirp.Repositories;
 
 public interface ICheepRepository
 {
     public List<CheepDto> GetCheeps(int page, int pageSize);
     public List<CheepDto> GetCheepsFromAuthor(string author, int page, int pageSize);
+    public Author GetAuthorByName(string authorName);
+    public Author GetAuthorByEmail(string email);
+    public void AddAuthor(string authorName, string email);
+    public void AddCheep(CheepDto cheepdto);
 }
