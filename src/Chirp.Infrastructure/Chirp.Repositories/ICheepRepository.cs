3225708688e1f78982f706804b6adf398e27827a
Victor <vitr@itu.dk>
2025-10-29 16:27:09 +0100

    Added 2 methods to be able to GetCheepsFromPage and GetCheepsFromAuthorAndPage

--- src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
@@ -6,6 +6,9 @@ public interface ICheepRepository
 {
     public List<CheepDto> GetCheeps(int page, int pageSize);
     public List<CheepDto> GetCheepsFromAuthor(string author, int page, int pageSize);
+    Task<IReadOnlyList<Cheep>> GetCheepsFromPage(int page,int pageSize, CancellationToken ct = default);
+    Task<IReadOnlyList<Cheep>> GetCheepsFromAuthorAndPage(string authorName, int page, int pageSize, CancellationToken ct = default);
+
     public Author GetAuthorByName(string authorName);
     public Author GetAuthorByEmail(string email);
     public void AddAuthor(string authorName, string email);
