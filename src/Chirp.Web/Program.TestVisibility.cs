59d53180fda4a7edd9a8702f0f32761150e12475
Victor <vitr@itu.dk>
2025-10-29 14:11:51 +0100

    Added tests folders and fixed sln + csproj files

---
+++ src/Chirp.Web/Program.TestVisibility.cs
@@ -0,0 +1,4 @@
+namespace Chirp.Web;
+
+// Enables WebApplicationFactory<Program> in tests without touching Program.cs
+public partial class Program {}
---
+++ test/UnitTest/Chirp.Core.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Chirp.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
