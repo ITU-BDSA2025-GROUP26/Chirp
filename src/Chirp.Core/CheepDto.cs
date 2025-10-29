017040eb57c0ca515e4dd9713f61f07d48b961da
Victor <vitr@itu.dk>
2025-10-29 15:43:35 +0100

    Fixed warnings by adding a required modifier or declaring the property as nullable

--- src/Chirp.Core/CheepDto.cs
+++ src/Chirp.Core/CheepDto.cs
@@ -2,8 +2,8 @@ namespace Chirp.Core;
 
 public class CheepDto
 {
-    public string Text { get; set; }
-    public string Author { get; set; }
-    public string TimeStamp { get; set; } //make a string, method for turning into datetime
-    public string AuthorEmail { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string TimeStamp { get; set; } = string.Empty;//make a string, method for turning into datetime
+    public string AuthorEmail { get; set; } = string.Empty;
 }
--- src/Chirp.Core/Models/Author.cs
+++ src/Chirp.Core/Models/Author.cs
@@ -2,8 +2,8 @@ namespace Chirp.Core.Models;
 
 public class Author
 {
-    public string Name { get; set; }
-    public string Email {get; set;}
+    public string Name { get; set; } = String.Empty;
+    public string Email {get; set;} = String.Empty;
     public int AuthorId { get; set;}
-    public ICollection<Cheep> Cheeps {get; set;}
+    public List <Cheep> Cheeps { get; set;} = new();
 }
