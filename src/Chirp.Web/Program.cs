ed8a940e74ea0d915c808ffc47177627f6fdb8ae
Nanna <nanhe@itu.dk>
2025-10-30 18:19:46 +0100

    Added a new migration after implementing Identity

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -17,8 +17,8 @@ builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
 
-//builder.Services.AddDefaultIdentity<Author>(options => options.SignIn.RequireConfirmedAccount = true)
-//    .AddEntityFrameworkStores<ChirpDBContext>();
+builder.Services.AddDefaultIdentity<Author>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ChirpDBContext>();
 
 var app = builder.Build();
 
