66bf0d9479d3951a1edc5250eafeb8ccd1513a0b
Carl <caps@itu.dk>
2025-11-12 15:32:55 +0100

    Changed to use Author.cs instead of a seperate class, added Followed user field to Author.cs

--- src/Chirp.Web/Areas/Identity/Data/ChirpUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Chirp.Razor.Areas.Identity.Data;
-
-public class ChirpUser : IdentityUser
-{
-    String[] FollowedUsers;
-}
--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Chirp.Infrastructure.Chirp.Service;
 using Chirp.Core;
+using Chirp.Razor.Areas.Identity.Data;
 
 namespace Chirp.Web.Pages
 {
@@ -18,7 +19,18 @@ namespace Chirp.Web.Pages
         public ActionResult OnGet(string author, [FromQuery] int page = 1)
         {
             int pageSize = 32;
-            Cheeps = _service.GetCheepsFromAuthor(author, page, pageSize);
+
+            if ()
+            {
+                Cheeps = _service.GetCheepsFromAuthor(author, page, pageSize);
+            }
+            else
+            {
+                for (String followed; )
+                {
+                    Cheeps.Add(_service.GetCheepsFromAuthor(followed, page, pagesize));
+                }
+            }
             return Page();
         }
     }
