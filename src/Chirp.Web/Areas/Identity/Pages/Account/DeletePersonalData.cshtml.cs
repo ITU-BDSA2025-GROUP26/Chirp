b1e2808638904b6987b04a8637672cebc99fac99
DavidKocuba <dkoc@itu.dk>
2025-11-26 15:16:23 +0100

    Changed the deletepersonaldata model, for an easier implementation, that works

--- src/Chirp.Web/Areas/Identity/Pages/Account/DeletePersonalData.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/DeletePersonalData.cshtml.cs
@@ -1,11 +1,11 @@
-using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft. AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Chirp.Core.Models;
 using Chirp.Infrastructure;
 
-namespace Chirp. Razor.Areas.Identity.Pages.Account
+namespace Chirp.Razor.Areas.Identity.Pages.Account
 {
     public class DeletePersonalDataModel : PageModel
     {
@@ -22,18 +22,6 @@ namespace Chirp. Razor.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _context = context;
         }
-        
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        public class InputModel
-        {
-            [Required]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
-        }
-
-        public bool RequirePassword { get; set; }
 
         public async Task<IActionResult> OnGet()
         {
@@ -43,7 +31,6 @@ namespace Chirp. Razor.Areas.Identity.Pages.Account
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            RequirePassword = await _userManager.HasPasswordAsync(user);
             return Page();
         }
 
@@ -54,30 +41,18 @@ namespace Chirp. Razor.Areas.Identity.Pages.Account
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
-
-            RequirePassword = await _userManager. HasPasswordAsync(user);
-            if (RequirePassword)
-            {
-                if (! await _userManager.CheckPasswordAsync(user, Input.Password))
-                {
-                    ModelState. AddModelError(string.Empty, "Incorrect password.");
-                    return Page();
-                }
-            }
             
-            var userCheeps = _context.Cheeps.Where(c => c. AuthorId == user.Id);
+            var userCheeps = _context.Cheeps.Where(c => c.AuthorId == user.Id);
             _context.Cheeps.RemoveRange(userCheeps);
             await _context.SaveChangesAsync();
             
             var result = await _userManager.DeleteAsync(user);
-            var userId = await _userManager.GetUserIdAsync(user);
-            if (! result.Succeeded)
+            if (!result.Succeeded)
             {
                 throw new InvalidOperationException($"Unexpected error occurred deleting user.");
             }
-
+            
             await _signInManager.SignOutAsync();
-
             return Redirect("~/");
         }
     }
