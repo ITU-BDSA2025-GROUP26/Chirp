23bc5abd6648d2ede8fb0e73cb81e75bad818ecd
Nick <nkch@itu.dk>
2025-12-03 13:50:47 +0100

    Added migrations for cheep  like toggling

---
+++ src/Chirp.Infrastructure/Migrations/20251203124738_AddCheepLikeTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCheepLikeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheepLikes",
+                columns: table => new
+                {
+                    CheepId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AuthorId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheepLikes", x => new { x.CheepId, x.AuthorId });
+                    table.ForeignKey(
+                        name: "FK_CheepLikes_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CheepLikes_Cheeps_CheepId",
+                        column: x => x.CheepId,
+                        principalTable: "Cheeps",
+                        principalColumn: "CheepId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheepLikes_AuthorId",
+                table: "CheepLikes",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheepLikes");
+        }
+    }
+}
