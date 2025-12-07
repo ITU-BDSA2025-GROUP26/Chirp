94f4491e69dec9a8de150529dbcaf285619c7bf6
Nanna <nanhe@itu.dk>
2025-12-07 13:59:40 +0100

    Added visibility modifiers to classes in Chirp.Repositories

--- src/Chirp.Infrastructure/Chirp.Repositories/IAuthorRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/IAuthorRepository.cs
@@ -4,13 +4,13 @@ namespace Chirp.Infrastructure.Chirp.Repositories;
 
 public interface IAuthorRepository
 {
-    Author GetAuthorByName(string authorName);
-    Author GetAuthorByEmail(string email);
-    void AddAuthor(string authorName, string email);
+    public Author GetAuthorByName(string authorName);
+    public Author GetAuthorByEmail(string email);
+    public void AddAuthor(string authorName, string email);
 
-    Task Follow(string followerUserName, string followeeUserName);
-    Task Unfollow(string followerUserName, string followeeUserName);
+    public Task Follow(string followerUserName, string followeeUserName);
+    public Task Unfollow(string followerUserName, string followeeUserName);
 
-    Task<List<Author>> GetFollowing(string userNameOrEmail);
-    Task<List<Author>> GetFollowers(string userNameOrEmail);
+    public Task<List<Author>> GetFollowing(string userNameOrEmail);
+    public Task<List<Author>> GetFollowers(string userNameOrEmail);
 }
